75f7dab19f6536c7fd1577c5c7930471724b9797
salat-23 <misikon@mail.ru>
2023-03-13 23:42:44 +0400

    PISS!!!!!!!!!

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,4 +26,15 @@ tasks.withType<Jar> {
     manifest {
         attributes["Main-Class"] = "me.salat23.piss.MainKt"
     }
+
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
