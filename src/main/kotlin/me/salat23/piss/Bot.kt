68bdc2f487de772da0dfd8afaa9d3993575d2520
salat-23 <misikon@mail.ru>
2023-03-14 11:26:03 +0400

    PISS!!!!!!!!!!

--- src/main/kotlin/me/salat23/piss/Bot.kt
+++ src/main/kotlin/me/salat23/piss/Bot.kt
@@ -2,9 +2,27 @@ package me.salat23.piss
 
 import api.longpoll.bots.LongPollBot
 import api.longpoll.bots.model.events.messages.MessageNew
+import java.io.File
+import kotlin.random.Random
 
-class Bot: LongPollBot() {
+class Bot() : LongPollBot() {
 
+    private val envToken = System.getenv("PISS_TOKEN")
+    private val dvachTexts: List<String>
+
+    init {
+        val path = this.javaClass.getResource("/2ch/")?.file
+        if (path != null) {
+            val texts = mutableListOf<String>()
+            val file = File(path)
+            file.walk().forEach { file ->
+                texts.addAll(file.readText().split("%~%~%"))
+            }
+            dvachTexts = texts.toList()
+        } else {
+            dvachTexts = listOf()
+        }
+    }
     private val keywords = listOf(
         "python",
         "pyhton",
@@ -13,14 +31,23 @@ class Bot: LongPollBot() {
         "питхон",
         "пэйтон",
         "питончик",
+        "питухон"
     )
-    override fun getAccessToken(): String {
-        return "vk1.a.zhXc_AJrtbl6zOSYLOowWmMTR7PTDBSWkFW4luT-Y2-QCsS6SFvD2JOoQoQxG2SFLDcGl7JgvYaYUprF8O_BG3LF0vuRFtUCttNlh-40ulb40ePWKrEji5agHRHE0TL95PWCbTBt5tzY4xFRD5urOEOpvbJ-ZRxmpsKKzEs-yAlBH8RI72huTJ9lNORAVndGqeuwtB8iW8rjUfZSq6L7wA"
-    }
+    override fun getAccessToken(): String = envToken
     override fun onMessageNew(messageNew: MessageNew?) {
         try {
             if (messageNew?.message?.hasText() == true) {
-                val text = messageNew.message.text!!
+                val text = messageNew.message.text!!.replace("[^A-Za-zА-Яа-я]", "")
+
+                if (text.startsWith("!2ch") && dvachTexts.isNotEmpty()) {
+                    vk.messages.send()
+                        .setPeerId(messageNew.message.peerId)
+                        .setReplyTo(messageNew.message.id)
+                        .setMessage(dvachTexts[Random.Default.nextInt(0, dvachTexts.size-1)])
+                        .execute()
+                    return
+                }
+
                 if (messageNew.message.fromId == 255136148 && !text.startsWith("!")) return
                 keywords.forEach { word ->
                     if (text.lowercase().contains(word.lowercase())) {
